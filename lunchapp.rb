a31f55d0b9fb98323a414cb06506f95acd3f4fa6
Patrick Berard <pberard@carthage.edu>
2016-03-22 23:09:33 -0500

    Cleaned up some code, improved behaviour when no results are returned, especially for when blacklists backfire.

--- lunchapp.rb
+++ lunchapp.rb
@@ -5,6 +5,16 @@ enable :logging
 
 configure do 
 	@@client = getClient()
+	#For the future this list may be replaced by the list of restaurant types located here: https://www.yelp.com/developers/documentation/v2/all_category_list
+	@@food = [
+			#Ethnic food types
+			"Indian", "Greek", "Thai", "Chinese", "Japanese", "American", "Armenian", "Peruvian", 
+			"Chilean", "Bolivian", "Ethiopian", "German", "Irish", "Brazilian", "Argentine", "Spanish", 
+			"Mexican", "French", "Italian", "Korean", "Southern", "Morroccan", "Vietnamese",
+			"Middle Eastern", "Lebenese", "Pakistani",
+			#Specific types of foods
+			"Seafood", "Pizza", "Steak", "Burgers", "BBQ", "Sushi", "Sandwhich"]
+	@@search_radius = 1610 #1 mile, 1609.34 meters
 end
 
 helpers do
@@ -50,34 +60,37 @@ helpers do
 		return true
 	end
 
+	def findMeFood(yelp_parms, useCoord, coords, where)
+		
+		if useCoord
+			#logger.info "Searching by coordinates"
+			yelp_parms[:radius] = @@search_radius
+		
+			return @@client.search_by_coordinates(coords, yelp_parms)
+		else
+			#logger.info "Searching by specific location: " + @where.to_s
+			
+			return @@client.search(@where, yelp_parms)
+		end
+	end
+
 end
 
 get '/' do
 	logger.info "**** Looking for lunch... ****"
-	logger.info params
-	#For the future this list may be replaced by the list of restaurant types located here: https://www.yelp.com/developers/documentation/v2/all_category_list
-	@food = [
-			#Ethnic food types
-			"Indian", "Greek", "Thai", "Chinese", "Japanese", "American", "Armenian", "Peruvian", 
-			"Chilean", "Bolivian", "Ethiopian", "German", "Irish", "Brazilian", "Argentine", "Spanish", 
-			"Mexican", "French", "Italian", "Korean", "Southern", "Morroccan", "Vietnamese",
-			"Middle Eastern", "Lebenese", "Pakistani",
-			#Specific types of foods
-			"Seafood", "Pizza", "Steak", "Burgers", "BBQ", "Sushi", "Sandwhich"]
+	@food = @@food
 
 	#Where are we searching?  First choice is geocoordinates, otherwise check if they specified a location.
 	#Otherwise ask them to specify a location.
 	useCoord = false
 	
 	if (checkCoords?(params[:lat], params[:long]))
-		logger.info "Using Coordinates: " + params[:lat].to_s + ", " + params[:long].to_s
 		coords = { 
 			latitude: params[:lat], 
 			longitude: params[:long] 
 		}
 		useCoord = true
 	elsif (!isNilOrEmpty?(params[:where])) 
-		logger.info "Using specific location: " + params[:where].to_s
 		@where = params[:where]
 	else 
 		logger.info "No location specified, defaulting to Chitown"
@@ -89,16 +102,13 @@ get '/' do
 	@what = ""
 
 	if params[:specificInput] == "true"
-		logger.info "Searching for a specific item: " + params[:specificInput].to_s
 		@what = params[:specific]
 	elsif params[:whiteListInput] == "true"
 		if !params[:arr].nil? && params[:arr].length > 0
-			logger.info "Searching via whitelist"
 			#Search using only the items that were checked.  Shuffle them up for variety!
 			@what = makeArraySearchable(params[:arr].shuffle!)
 		else 
 			#User chose to whitelist food options but then didn't specify any choices...
-			logger.info "Whitelist length was 0, searching for whatever is closest"
 			#Blank input will search for anything and everything
 			@what = ""
 		end
@@ -109,24 +119,22 @@ get '/' do
 			#Subtract what the user checked from the list of all foods to get our searchable list
 			blacklist = @food - params[:arr]
 			
-			@what = makeArraySearchable (blacklist.shuffle!)
+			@what = makeArraySearchable(blacklist.shuffle!)
 		else
-			logger.info "Blacklist length was 0, searching for whatever is closest"
-			#Blank input will search for anything and everything
+			#User chose to blacklist but didn't check anything...So search for everything!
 			@what = ""
 		end
 	else
 		#Blank input will search for anything and everything
-		logger.info "Searching for anything and everything"
 		@what = ""
 	end
 
 	#How will we sort? #0=Best matched (default), 1=Distance, 2=Highest Rated
-	#@randSort = 1
-	@randSort = rand(3)  #Choose this to be EXTRA random!
+	@randSort = 1 #Choose closest options first
+	#@randSort = rand(3)  #Choose this to be EXTRA random!
 
-	#Search yelp!
-	parms = {
+	#Build paramters to search yelp!
+	yelp_parms = {
 		category_filter: 'restaurants', 
 		term: @what, #add additional filters in order separated by ','
 		limit: 10,
@@ -134,24 +142,17 @@ get '/' do
 	} 
 	
 	#Make the call to Yelp
-	if useCoord
-		logger.info "Searching by coordinates"
-		parms[:radius] = 1610 #1 mile, 1609.34 meters
+	@retval = findMeFood(yelp_parms, useCoord, coords, @where)
 	
-		@retval = @@client.search_by_coordinates(coords, parms)
-	else
-		logger.info "Searching by specific location: " + @where.to_s
-		
-		@retval = @@client.search(@where, parms)
-	end
-	#Shuffle the list to change it up
-	@retval.businesses.shuffle!
+	#Shuffle the list to change it up?
+	#@retval.businesses.shuffle!
 	
 	#In case theres a flop and no results come back
 	if @retval.businesses.length < 1
 		#logger.info "Didn't find anything, rerouting to default"
-		@error_message = "Swing and a miss!<br> We couldn't find anything in your area that matches what you want. :["
-		#redirect to('/')
+		@error_message = "Swing and a miss!<br> We couldn't find anything in your area that matches what you wanted. :[<br> Try some of these suggestions instead?"
+		yelp_parms["term"] = "" #Make sure we're just getting whatever is closest
+		@retval = findMeFood(yelp_parms, useCoord, coords, @where)
 	end
 
 	logger.info "**** Lunch has been found ****"
